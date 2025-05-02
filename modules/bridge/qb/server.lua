dec9f5806d7573f6062e1277f986054d496c3bc5
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-05-02 17:41:24 -0400

    fix for current qb

--- modules/bridge/qb/server.lua
+++ modules/bridge/qb/server.lua
@@ -99,8 +99,10 @@ SetTimeout(500, function()
 end)
 
 function server.UseItem(source, itemName, data)
-    local cb = QBCore.Functions.CanUseItem(itemName)
-    return cb and cb(source, data)
+    local itemData = QBCore.Functions.CanUseItem(itemName)
+    if type(itemData) == "table" and itemData.func then
+        itemData.func(source, data)
+    end
 end
 
 AddEventHandler('QBCore:Server:OnMoneyChange', function(src, account, amount, changeType)
